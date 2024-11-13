4d93e97e6f18ee019129da269b61257ef26efb2c
Redando Dieujuste <101023464+9308RD00@users.noreply.github.com>
2024-11-13 14:18:06 +0000

    new8

--- src/app.py
+++ src/app.py
@@ -10,9 +10,39 @@ from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
 #from models import Person
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+users = [
+    { "username": "9308RD00", "password": "Redando", "email": "redando.dieujuste00@gmail.com" },
+]
+
+@app.route('/users', methods=['GET'])
+def get_user():
+    json_text = jsonify(users)
+    return json_text
+
+
+@app.route('/users', methods=['POST'])
+def post_user():
+    request_body = request.json
+    users.append(request_body)
+    return jsonify(users)
+
+
+@app.route('/users/<int:position>', methods=['PUT'])
+def edit_user(position):
+    request_body = request.json
+    users[position] = request_body
+    return jsonify(users)
+
+
+@app.route('/users/<int:position>', methods=['DELETE'])
+def delete_user(position):
+    print( position)
+    deleted = users.pop(position)
+    return jsonify(users)    
+
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
